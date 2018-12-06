1743c29e1ccc51e365bc4104f58f58c8dfc7756f
Vojto Bálint <38936748+vojtob@users.noreply.github.com>
2018-12-06 21:29:09 +0100

    ignore img files

--- img_BW/_BW_subdirs.bat
+++ img_BW/_BW_subdirs.bat
@@ -1,2 +1,2 @@
+FOR /R %%c in (*.png) DO call convert.exe -colorspace Gray "%%~fc" "%%~pc/BW/%%~nc-BW.png"
 FOR /R %%c in (*.jpg) DO call convert.exe -colorspace Gray "%%~fc" "%%~pc/BW/%%~nc-BW.png"
-REM FOR /R %%c in (*.png) DO call convert.exe -colorspace Gray "%%~fc" "%%~pc/BW/%%~nc-BW.png"
